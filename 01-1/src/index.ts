d9569936f7d0302e99d900a5a9b72a4637ddaba9
Alan Russell <ajrussellaudio@gmail.com>
2025-12-01 18:08:08 +0000

    Add 01-1 solution

--- 01-1/src/index.ts
+++ 01-1/src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hiya');
+import { createReadStream } from "node:fs";
+import path from "node:path";
+import { createInterface } from "node:readline";
+import { rotate } from "./rotate";
+
+const __dirname = path.resolve();
+
+const inputStream = createReadStream(path.join(__dirname, "src", "input.txt"));
+
+const rl = createInterface({
+  input: inputStream,
+  crlfDelay: Infinity,
+});
+
+let position = 50;
+let zeroes = 0;
+
+rl.on("line", (line) => {
+  const newPosition = rotate(position, line);
+  if (newPosition === 0) {
+    zeroes += 1;
+  }
+  position = newPosition;
+});
+
+rl.on("close", () => {
+  console.log("zeroes:", zeroes);
+});
