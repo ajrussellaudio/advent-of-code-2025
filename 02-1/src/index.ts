d3bb1f865fada6238d847da0a4c0b5e08ebd5a26
Alan Russell <ajrussellaudio@gmail.com>
2025-12-02 14:56:45 +0000

    Add runner, get solution

--- 02-1/src/index.ts
+++ 02-1/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hiya');
+import { readFile } from "fs";
+import path from "path";
+import { sumIds } from "./sum-ids";
+import { filterRangeToInvalid } from "./filter-range-to-invalid";
+
+const inputPath = path.join(__dirname, "input.txt");
+readFile(inputPath, { encoding: "utf-8" }, (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const total = data.split(",").reduce((prev, range) => {
+    const rangeInvalidTotal = sumIds(filterRangeToInvalid(range));
+    return prev + rangeInvalidTotal;
+  }, 0);
+  console.log("total:", total);
+});
