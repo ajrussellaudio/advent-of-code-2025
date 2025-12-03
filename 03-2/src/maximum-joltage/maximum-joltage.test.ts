b0050bf61b6b4157514d9f5d529de5723a520f66
Alan Russell <ajrussellaudio@gmail.com>
2025-12-03 14:24:41 +0000

    Refactor maximumJoltage to work with any battery count

--- 03-2/src/maximum-joltage/maximum-joltage.test.ts
+++ 03-2/src/maximum-joltage/maximum-joltage.test.ts
@@ -7,7 +7,20 @@ describe("maximumJoltage", () => {
     ${"811111111111119"} | ${89}
     ${"234234234234278"} | ${78}
     ${"818181911112111"} | ${92}
-  `("$bank returns $maximum", ({ bank, maximum }) => {
-    expect(maximumJoltage(bank)).toBe(maximum);
+  `("$bank returns $maximum, selecting 2 batteries", ({ bank, maximum }) => {
+    expect(maximumJoltage(bank, 2)).toBe(maximum);
   });
+
+  it.each`
+    bank                 | maximum
+    ${"987654321111111"} | ${987654321111}
+    ${"811111111111119"} | ${811111111119}
+    ${"234234234234278"} | ${434234234278}
+    ${"818181911112111"} | ${888911112111}
+  `(
+    "$bank returns $maximum, selecting 12 batteries (default)",
+    ({ bank, maximum }) => {
+      expect(maximumJoltage(bank)).toBe(maximum);
+    },
+  );
 });
--- 03-2/src/maximum-joltage/maximum-joltage.ts
+++ 03-2/src/maximum-joltage/maximum-joltage.ts
@@ -5,11 +5,14 @@ function concat(...args: number[]) {
   return parseInt(args.join(""));
 }
 
-export function maximumJoltage(bank: string) {
-  const batteries = parseBank(bank);
-  const firstMaximum = findLargest(batteries.slice(0, -1));
-  const secondMaximum = findLargest(
-    batteries.slice(batteries.indexOf(firstMaximum) + 1),
-  );
-  return concat(firstMaximum, secondMaximum);
+export function maximumJoltage(bank: string, batteriesCount = 12) {
+  let batteries = parseBank(bank);
+  const maximums: number[] = [];
+  for (let i = batteriesCount; i > 0; i--) {
+    const endIndex = batteries.length - i + 1;
+    const maximum = findLargest(batteries.slice(0, endIndex));
+    maximums.push(maximum);
+    batteries = batteries.slice(batteries.indexOf(maximum) + 1);
+  }
+  return concat(...maximums);
 }
