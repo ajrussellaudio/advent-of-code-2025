5a8aeb0139d08ff4bbd37ce551c679ca55878d08
Alan Russell <ajrussellaudio@gmail.com>
2025-12-04 00:26:15 +0000

    Add --test flag to mkfn

--- mkfn.sh
+++ mkfn.sh
@@ -7,9 +7,38 @@ FN_PATH=$1 || "."
 NAME=$(basename $FN_PATH)
 NAME_SNAKE=$(echo $NAME | perl -pe 's/(-)./uc($&)/ge;s/-//g')
 
+shift 
+
+WITH_TEST=false
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -t | --test)
+      WITH_TEST=true
+      ;;
+    --no-test)
+      WITH_TEST=false
+      ;;
+    *)
+      echo "Invalid option: $1" >&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 echo "Creating $FN_PATH"
 mkdir $FN_PATH
 
+FN_FILE="$FN_PATH/$NAME.ts"
+echo "Creating $FN_FILE"
+cat > "$FN_FILE" <<- EOF
+export function $NAME_SNAKE() {
+  return null;
+}
+EOF
+
+if $WITH_TEST; then
 TEST_FILE="$FN_PATH/$NAME.test.ts"
 echo "Creating $TEST_FILE"
 cat > "$TEST_FILE" <<- EOF
@@ -21,14 +50,8 @@ describe('$NAME_SNAKE', () => {
   })
 })
 EOF
+fi
 
-FN_FILE="$FN_PATH/$NAME.ts"
-echo "Creating $FN_FILE"
-cat > "$FN_FILE" <<- EOF
-export function $NAME_SNAKE() {
-  return null;
-}
-EOF
 
 INDEX_FILE="$FN_PATH/index.ts"
 echo "Creating $INDEX_FILE"
