473ecfe52deb8aa32a65dbd3b9ec772337c547f8
Alan Russell <ajrussellaudio@gmail.com>
2025-12-04 16:58:37 +0000

    Day 04 (#2)
    * Stub 04-1
    
    * Add Grid class
    
    * Add 04-1 solution
    
    * Stub 04-2
    
    * Add print method
    
    * Add remove method
    
    * Refactor remove method to return a copy
    
    * Add removeAll fn
    
    * Add 04-2 solution
    
    * [Copilot] Avoid serialising to make a copy
    
    * Remove print method
    
    * [Copilot] Remove debug method

---
+++ 04-2/src/grid/grid.test.ts
@@ -0,0 +1,104 @@
+import { Grid } from "./grid";
+
+describe("Grid", () => {
+  it("can be initialised", () => {
+    const rawGrid = `
+    ..@
+    .@.
+    @@.
+    `;
+    expect(new Grid(rawGrid)).not.toBeNull();
+  });
+
+  describe("getSquare", () => {
+    let grid: Grid;
+
+    beforeEach(() => {
+      const rawGrid = `
+      ..@
+      .@.
+      @@.
+      `;
+      grid = new Grid(rawGrid);
+    });
+
+    it.each`
+      x    | y    | square
+      ${0} | ${0} | ${"."}
+      ${1} | ${0} | ${"."}
+      ${2} | ${0} | ${"@"}
+      ${0} | ${1} | ${"."}
+      ${1} | ${1} | ${"@"}
+      ${2} | ${1} | ${"."}
+      ${0} | ${2} | ${"@"}
+      ${1} | ${2} | ${"@"}
+      ${2} | ${2} | ${"."}
+    `("returns a valid square, ($x, $y) = $square", ({ x, y, square }) => {
+      expect(grid.getSquare(x, y)).toBe(square);
+    });
+  });
+
+  describe("checkSquareIsAccessible", () => {
+    let grid: Grid;
+
+    beforeEach(() => {
+      const rawGrid = `
+      .@@
+      @.@
+      @@@
+      `;
+      grid = new Grid(rawGrid);
+    });
+
+    it.each`
+      x    | y    | isAccessible
+      ${0} | ${0} | ${false}
+      ${1} | ${0} | ${true}
+      ${2} | ${0} | ${true}
+      ${0} | ${1} | ${true}
+      ${1} | ${1} | ${false}
+      ${2} | ${1} | ${false}
+      ${0} | ${2} | ${true}
+      ${1} | ${2} | ${false}
+      ${2} | ${2} | ${true}
+    `("square ($x, $y) returns $isAccessible", ({ x, y, isAccessible }) => {
+      expect(grid.checkSquareIsAccessible(x, y)).toBe(isAccessible);
+    });
+  });
+
+  describe("totalIsAccessible", () => {
+    let grid: Grid;
+
+    beforeEach(() => {
+      const rawGrid = `
+      .@@
+      @.@
+      @@@
+      `;
+      grid = new Grid(rawGrid);
+    });
+
+    it("returns the total number of accessible squares", () => {
+      expect(grid.totalIsAccessible()).toBe(5);
+    });
+  });
+
+  describe("remove", () => {
+    let grid: Grid;
+
+    beforeEach(() => {
+      const rawGrid = `
+      .@@
+      @.@
+      @@@
+      `;
+      grid = new Grid(rawGrid);
+    });
+
+    it("removes all accessible rolls, returning a copy", () => {
+      const removed = grid.remove();
+      expect(removed.totalIsAccessible()).toEqual(2);
+      expect(grid.totalIsAccessible()).toBe(5);
+    });
+  });
+});
---
+++ 04-2/src/grid/grid.ts
@@ -0,0 +1,71 @@
+export class Grid {
+  private readonly grid: string[][];
+
+  constructor(grid: string | string[][]) {
+    if (Array.isArray(grid)) {
+      this.grid = grid;
+    } else {
+      this.grid = grid
+        .trim()
+        .split("\n")
+        .map((row) => row.trim().split(""));
+    }
+  }
+
+  getSquare(x: number, y: number) {
+    return (this.grid[y] || this.emptyRow())[x] ?? ".";
+  }
+
+  checkSquareIsAccessible(x: number, y: number) {
+    if (this.getSquare(x, y) === ".") {
+      return false;
+    }
+    const subgrid = this.squareWithNeighbours(x, y);
+    const filledNeighbours = subgrid.reduce((prevRowTotal, row, _y) => {
+      const rowTotal = row.reduce((rowTotal, square, _x) => {
+        if (_x === 1 && _y === 1) {
+          return rowTotal;
+        }
+        return rowTotal + (square === "@" ? 1 : 0);
+      }, 0);
+      return prevRowTotal + rowTotal;
+    }, 0);
+    return filledNeighbours < 4;
+  }
+
+  totalIsAccessible() {
+    return this.grid.reduce((total, row, y) => {
+      return (
+        total +
+        row.reduce((rowTotal, _square, x) => {
+          return rowTotal + (this.checkSquareIsAccessible(x, y) ? 1 : 0);
+        }, 0)
+      );
+    }, 0);
+  }
+
+  remove() {
+    const prevGrid = new Grid(this.grid.map((row) => [...row]));
+    const removed = this.grid.map((row, y) => {
+      return row.map((square, x) =>
+        prevGrid.checkSquareIsAccessible(x, y) ? "." : square,
+      );
+    });
+    return new Grid(removed);
+  }
+
+  private emptyRow() {
+    return Array.from(this.grid[0]).map(() => ".");
+  }
+
+  private squareWithNeighbours(x: number, y: number) {
+    const subgrid: string[][] = [];
+    for (let yOff = -1; yOff <= 1; yOff++) {
+      subgrid[yOff + 1] = [];
+      for (let xOff = -1; xOff <= 1; xOff++) {
+        subgrid[yOff + 1][xOff + 1] = this.getSquare(x + xOff, y + yOff);
+      }
+    }
+    return subgrid;
+  }
+}
