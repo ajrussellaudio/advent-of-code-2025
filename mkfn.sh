275bbcd138ea4f7684441a82d0c9bfab569e8d30
Alan Russell <ajrussellaudio@gmail.com>
2025-12-04 23:48:58 +0000

    Format mkfn.sh

--- mkfn.sh
+++ mkfn.sh
@@ -7,22 +7,22 @@ FN_PATH=$1 || "."
 NAME=$(basename $FN_PATH)
 NAME_SNAKE=$(echo $NAME | perl -pe 's/(-)./uc($&)/ge;s/-//g')
 
-shift 
+shift
 
 WITH_TEST=false
 
 while [ $# -gt 0 ]; do
   case $1 in
-    -t | --test)
-      WITH_TEST=true
-      ;;
-    --no-test)
-      WITH_TEST=false
-      ;;
-    *)
-      echo "Invalid option: $1" >&2
-      exit 1
-      ;;
+  -t | --test)
+    WITH_TEST=true
+    ;;
+  --no-test)
+    WITH_TEST=false
+    ;;
+  *)
+    echo "Invalid option: $1" >&2
+    exit 1
+    ;;
   esac
   shift
 done
@@ -32,16 +32,16 @@ mkdir $FN_PATH
 
 FN_FILE="$FN_PATH/$NAME.ts"
 echo "Creating $FN_FILE"
-cat > "$FN_FILE" <<- EOF
+cat >"$FN_FILE" <<-EOF
 export function $NAME_SNAKE() {
   return null;
 }
 EOF
 
 if $WITH_TEST; then
-TEST_FILE="$FN_PATH/$NAME.test.ts"
-echo "Creating $TEST_FILE"
-cat > "$TEST_FILE" <<- EOF
+  TEST_FILE="$FN_PATH/$NAME.test.ts"
+  echo "Creating $TEST_FILE"
+  cat >"$TEST_FILE" <<-EOF
 import { $NAME_SNAKE } from "./$NAME"
 
 describe('$NAME_SNAKE', () => {
@@ -52,10 +52,8 @@ describe('$NAME_SNAKE', () => {
 EOF
 fi
 
-
 INDEX_FILE="$FN_PATH/index.ts"
 echo "Creating $INDEX_FILE"
-cat > "$INDEX_FILE" <<- EOF
+cat >"$INDEX_FILE" <<-EOF
 export { $NAME_SNAKE } from "./$NAME";
 EOF
-
