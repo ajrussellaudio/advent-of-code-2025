4d6ed0d5a89986d9be35e7cd67d8d0f1831c6412
Alan Russell <ajrussellaudio@gmail.com>
2025-12-05 13:56:59 +0000

    Read inputs as ranges and ingredients

--- 05-1/src/index.ts
+++ 05-1/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hiya');
+import path from "node:path";
+import { readInput } from "utils";
+
+const inputPath = path.join(__dirname, "input.txt");
+readInput(inputPath, (data) => {
+  const [ranges, ingredients] = data
+    .split("\n\n")
+    .map((chunk) => chunk.split("\n"));
+  console.dir(ranges.slice(0, 5));
+  console.dir(ingredients.slice(0, 5));
+});
