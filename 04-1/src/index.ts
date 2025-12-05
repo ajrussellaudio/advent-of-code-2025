3d2bb18b66258488b01ded448de804d690c1f332
Alan Russell <ajrussellaudio@gmail.com>
2025-12-05 13:50:55 +0000

    Add utils pkg with readInput fn

--- 04-1/src/index.ts
+++ 04-1/src/index.ts
@@ -1,13 +1,9 @@
-import { readFile } from "fs";
 import path from "path";
 import { Grid } from "./grid";
+import { readInput } from "utils";
 
 const inputPath = path.join(__dirname, "input.txt");
-readFile(inputPath, { encoding: "utf-8" }, (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+readInput(inputPath, (data) => {
   const grid = new Grid(data);
   const total = grid.totalIsAccessible();
   console.log("total:", total);
