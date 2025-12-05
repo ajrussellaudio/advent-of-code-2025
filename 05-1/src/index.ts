ef077af526df30084874bab827638388f85219df
Alan Russell <ajrussellaudio@gmail.com>
2025-12-05 14:19:39 +0000

    Add 05-1 solution

--- 05-1/src/index.ts
+++ 05-1/src/index.ts
@@ -1,11 +1,12 @@
 import path from "node:path";
 import { readInput } from "utils";
+import { countFresh } from "./count-fresh";
 
 const inputPath = path.join(__dirname, "input.txt");
 readInput(inputPath, (data) => {
   const [ranges, ingredients] = data
     .split("\n\n")
     .map((chunk) => chunk.split("\n"));
-  console.dir(ranges.slice(0, 5));
-  console.dir(ingredients.slice(0, 5));
+  const freshCount = countFresh(ingredients, ranges);
+  console.log("freshCount:", freshCount);
 });
