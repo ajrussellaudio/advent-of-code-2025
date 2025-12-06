7ec4ed23b9e1ead63598b39b1e0881b440876f25
Alan Russell <ajrussellaudio@gmail.com>
2025-12-06 11:16:56 +0000

    Add 06-1 solution

--- 06-1/src/index.ts
+++ 06-1/src/index.ts
@@ -1,13 +1,11 @@
 import path from "node:path";
 import { readInput } from "utils";
 import { parseInput } from "./parse-input";
+import { doAllTheMaths } from "./do-all-the-maths";
 
 const inputFile = path.join(__dirname, "input.txt");
 readInput(inputFile, (data) => {
-  const { operators, operands } = parseInput(data);
-  console.log(
-    "operands:",
-    operands.map((list) => list.length),
-  );
-  console.log("operators:", operators.length);
+  const input = parseInput(data);
+  const grandTotal = doAllTheMaths(input);
+  console.log("grandTotal:", grandTotal);
 });
