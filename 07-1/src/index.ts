166de9874be97f074d1159ef4d5b048778abbbbe
Alan Russell <ajrussellaudio@gmail.com>
2025-12-07 10:12:57 +0000

    Get 07-1 solution

--- 07-1/src/index.ts
+++ 07-1/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hiya');
+import path from "node:path";
+import { readInput } from "utils";
+import { TachyonManifold } from "./tachyon-manifold/tachyon-manifold";
+
+const inputFile = path.join(__dirname, "input.txt");
+readInput(inputFile, (data) => {
+  const tachyonManifold = new TachyonManifold(data);
+  const splits = tachyonManifold.countSplits();
+  console.log("splits:", splits);
+});
